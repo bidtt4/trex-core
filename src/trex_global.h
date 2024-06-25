8e7e0efdb135a7e81ad62fa33fb24a6382a11c12
Adrian Byström <adrian.a.bystrom@ericsson.com>
2024-06-25 15:20:55 +0200

    Toggle for GARP requests in pretest
    Adds toggle to trex-core for toggling the usage
    of GARP requests in pretest on and off.
    Default behaviour is as before, the parameter when set
    ignores GARPs during pretest.

--- src/trex_global.h
+++ src/trex_global.h
@@ -636,6 +636,7 @@ public:
         m_tunnel_loopback = false;
         m_tunnel_enabled = false;
         m_rx_dp_ring_size = 0;
+        m_garp_ignore = false;
     }
 
     CParserOption(){
@@ -709,6 +710,7 @@ public:
     bool            m_astf_best_effort_mode;
     bool            m_tunnel_loopback;
     uint16_t        m_rx_dp_ring_size;              // Size of rings between Dp and Rx.
+    bool            m_garp_ignore;
 
 
 public:
